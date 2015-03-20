066984880767e5266e984d40a67699120e45dfa6
Jack-Benny Persson <jack-benny@cyberinfo.se>
2015-03-20 20:05:57 +0100

    Started coding on Python script

--- makepdf.py
+++ makepdf.py
@@ -2,17 +2,44 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from subprocess import call
 
 def main():
+    
+    # Define some variables
+    outfile = "out.pdf" # Default output file
+    with_title = "/usr/share/mdocu/with-titlepage.tex" # Template with titlepage
+    without_title = "/usr/share/mdocu/without-titlepage.tex" # Template without
+                                                             # titlepage
+
+    # Parse arguments for the script
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="the markdown-file you wish to convert")
-    parser.add_argument("--no-toc", help="don't make any table of contents",
+    parser.add_argument("--notoc", help="don't make any table of contents",
         action="store_true")
-    parser.add_argument("--no-title", help="don't include a titlepage",
+    parser.add_argument("--notitle", help="don't include a titlepage",
         action="store_true")
+    parser.add_argument("-o", "--output", type=str, 
+        help="save file into <file>")
     args = parser.parse_args()
-    
-    print args.filename
+
+    # Set output file to whatever was entered at the -o option
+    if args.output:
+        outfile = args.output
+
+    # Iterate through the formating options    
+    if args.notoc == True and args.notitle == False:
+       call(["pandoc", "-N", "--template=" + with_title, 
+            args.filename, "--filter", "pandoc-citeproc", "-o", outfile]) 
+    elif args.notoc == True and args.notitle == True:
+       call(["pandoc", "-N", "--template=" + without_title, 
+            args.filename, "--filter", "pandoc-citeproc", "-o", outfile])
+    elif args.notitle == True and args.notoc == False:
+       call(["pandoc", "-N", "--template=" + without_title, "--toc",
+            args.filename, "--filter", "pandoc-citeproc", "-o", outfile])
+    else:
+       call(["pandoc", "-N", "--template=" + with_title, "--toc",
+            args.filename, "--filter", "pandoc-citeproc", "-o", outfile])
 
 if __name__=='__main__':
     main()
